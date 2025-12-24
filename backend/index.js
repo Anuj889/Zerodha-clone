b7d9dd80e7665112775b791f3e0844736e7e0c64
Anuj Yadav <anujyadav242004@gmail.com>
2025-12-24 10:02:42 +0530

    Fix CORS for deployed frontend and

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
-app.options("*", cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
